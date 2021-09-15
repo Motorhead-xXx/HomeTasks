81c158f163deff590efa98cf895236a8a826a040
Motorhead <masa_i@bk.ru>
2021-09-16 01:02:49 +0300

    Updates

---
+++ precache-manifest.3454a35867bf7e22a51ad9960c0e5e00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211dc547761a41a6c5a5d512124c1d06",
+    "url": "/HomeTasks/index.html"
+  },
+  {
+    "revision": "393e82c78c567ecc98d0",
+    "url": "/HomeTasks/static/css/main.5ece82e8.chunk.css"
+  },
+  {
+    "revision": "8d510b2a82ea55ba8e6d",
+    "url": "/HomeTasks/static/js/2.cf78121a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeTasks/static/js/2.cf78121a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393e82c78c567ecc98d0",
+    "url": "/HomeTasks/static/js/main.5949acea.chunk.js"
+  },
+  {
+    "revision": "fc9da1f14a51a7ac31be",
+    "url": "/HomeTasks/static/js/runtime-main.6241284f.js"
+  },
+  {
+    "revision": "95ff61a0edfd29bf03d2c06994967113",
+    "url": "/HomeTasks/static/media/5.95ff61a0.png"
+  }
+]);
