57e4d7e284b1286fac16a76ec37363578f229563
Motorhead <masa_i@bk.ru>
2021-09-16 18:45:39 +0300

    5 task completed

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from "./pages/Routes.module.css"
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.link}>
+            <NavLink to={'/'} className={s.preJunior}>pre-Junior</NavLink>
+
+            <NavLink to={'/junior'} >Junior</NavLink>
+
+            <NavLink to={'/plus-junior'}>Junior+</NavLink>
+
+            <div className={s.bord}></div>
 
         </div>
     )
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import HW2 from "../../h2/HW2";
+
+
+
+function Junior() {
+    return (
+        <div>
+            <HW2/>
+        </div>
+    )
+}
+
+export default Junior
