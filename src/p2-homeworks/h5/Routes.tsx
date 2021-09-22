3b6c4d43f4554429912be9902021fcb9a0ed2199
Motorhead <masa_i@bk.ru>
2021-09-22 21:19:39 +0300

    Error completed

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Redirect, Route, Switch} from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Error404 from "./pages/Error404";
 import PreJunior from "./pages/PreJunior";
 import PlusJunior from "./pages/PlusJunior";
@@ -20,15 +20,15 @@ function Routes() {
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
                 {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-                <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+                <Route exact path={'/'} render={() => <Redirect to={PATH.PRE_JUNIOR} />} />
 
-                <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
-                <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
-                <Route path={PATH.PLUS_JUNIOR} render={() => <PlusJunior/>}/>
+                <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior />} />
+                <Route path={PATH.JUNIOR} render={() => <Junior />} />
+                <Route path={PATH.PLUS_JUNIOR} render={() => <PlusJunior />} />
                 // add routes
 
                 {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                <Route render={() => <Error404/>}/>
+                <Route render={() => <Error404 />} />
 
             </Switch>
         </div>
